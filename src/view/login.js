5e15b20dcbd3d0cc733aa4ea902f1a5ef6cfc402
nesnip <hpmarines12@gmail.com>
2019-12-06 17:56:00 -0500

    borrando archivo vacío

--- src/view/login.js
+++ src/view/login.js
@@ -1,15 +1,14 @@
-export default () =>{
-    const form = document.createElement('form');
-    const formContent = `
+export default () => {
+  const form = document.createElement('form');
+  const formContent = `
     <form class="login-container">
     <h2> FitnesSpace </h2>
     <input type="text"> Email
     <input type="text"> Password
     <button> Sign In </button>
     </form>`;
-    divElemt.classList.add('position')
-    form.innerHtml = formContent;
-    return form;
-
-
+  divElemt.classList.add('position');
+  form.innerHtml = formContent;
+  return form;
 }
+;
